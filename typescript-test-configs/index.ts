a4d660b6ce2f33a910684a8c797607c7ce85190e
Ethan <ethan.cho1826@gmail.com>
2022-09-20 15:10:12 -0500

    more comments on compilier options

--- typescript-test-configs/index.ts
+++ typescript-test-configs/index.ts
@@ -10,6 +10,8 @@ const norma: Chicken = {
     name: "Norma",
 };
 
+// tsc --init to create a config json file for typescript compilier
+
 // tsc --watch <file> to continuously compile any changes in whatever file was passed as an argument
 
 // tsc command will compile any and all .ts files that it can see at it's current directory and children directories
